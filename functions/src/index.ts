054b3dd7e94e04ae82e8334f2f94782731a16f3a
Samarth Kolanupaka <samarth212@gmail.com>
2026-02-08 12:15:59 -0500

    emulators

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,3 +8,21 @@ export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+/*
+next steops:
+
+Write your function(s) in functions/src/ and export from index.ts.
+Validate locally:
+npm --prefix functions run lint
+npm --prefix functions run build
+Start emulators from repo root:
+firebase emulators:start
+Test your HTTP function locally (example):
+http://127.0.0.1:5001/seedmoney-15f8e/us-central1/helloWorld
+In another terminal, run your app:
+npm run dev
+When behavior is correct, deploy:
+firebase deploy --only functions
+
+*/
