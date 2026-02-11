72b237e98747aa241de26427960644d2387e9018
Aneesh Reddy <beabravedude@gmail.com>
2026-02-11 18:44:03 -0500

    Made the page a client component and refactored MUI components to use className with Tailwind instead of sx.

--- app/kickoff-assignment/aneesh-reddy/page.tsx
+++ app/kickoff-assignment/aneesh-reddy/page.tsx
@@ -1,14 +1,15 @@
+'use client';
 import React from 'react';
 import {Button, Card, CardContent, Container, Divider, Typography} from "@mui/material";
 
-export default async function Page() {
+export default function Page() {
     return (
-        <Container maxWidth="lg" sx={{ mt: 8, }}>
+        <Container maxWidth="lg" className="mt-8">
             <Card>
                 <CardContent>
-                    <Typography variant="h1" textAlign="center" sx={{ letterSpacing: '8px'}}>Aneesh Reddy</Typography>
+                    <Typography variant="h1" textAlign="center">Aneesh Reddy</Typography>
                     <Typography variant="h5" textAlign="center" gutterBottom>Engineer @ SeedMoney</Typography>
-                    <Divider sx={{ my: 2, }}/>
+                    <Divider className="my-2" />
                     <Typography gutterBottom>Hello! I am <b>Aneesh</b> and I am an engineer on the SeedMoney team for Hack4Impact!</Typography>
                     <Button variant="contained" size="large" color="secondary">Do Absolutely Nothing</Button>
                 </CardContent>
