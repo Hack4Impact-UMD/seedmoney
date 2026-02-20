5a958824642980d381a4926feba6c721f6858861
Samarth Kolanupaka <samarth212@gmail.com>
2026-02-19 22:43:12 -0500

    handle null check

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const url = process.env.SUPABASE_URL;
 const anonKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(url!, anonKey!);
+if (!url || !anonKey) {
+  throw new Error(
+    "missing Supabase env variables, SUPABASE_URL and SUPABASE_ANON_KEY must be set.",
+  );
+}
+
+export const supabase = createClient(url, anonKey);
