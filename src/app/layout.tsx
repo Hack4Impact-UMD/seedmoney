4e88078dcfb818588db889b024de38ea9f54d024
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-02-21 14:26:27 -0500

    Updated with Lato Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,61 @@
-import type { Metadata } from "next";
-import { Lato } from "next/font/google"; 
+import localFont from "next/font/local";
 import "./globals.css";
 
-const lato = Lato({
-    subsets: ["latin"],
-    weight: ["700"],
+const lato = localFont({
+  src: [
+    {
+      path: "../fonts/Lato/Lato-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Lato/Lato-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Lato/Lato-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Lato/Lato-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Lato/Lato-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Lato/Lato-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Lato/Lato-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../fonts/Lato/Lato-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+
+    {
+      path: "../fonts/Lato/Lato-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/Lato/Lato-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+  variable: "--font-Lato",
 });
 
 export const metadata = {
@@ -20,7 +71,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${lato} ${lato} antialiased`}
+        className={`${lato.className} antialiased`}
       >
         {children}
       </body>
