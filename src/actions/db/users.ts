a0922d1bacd52ec4e751e267dcacb1c989de8a77
Samarth Kolanupaka <samarth212@gmail.com>
2026-03-01 13:45:52 -0500

    return error message

--- src/actions/db/users.ts
+++ src/actions/db/users.ts
@@ -1,40 +1,47 @@
-import {Users} from "@/src/types";
-import {supabase} from "@/src/lib/supabase-client";
+import { Users } from "@/src/types";
+import { supabase } from "@/src/lib/supabase-client";
 
 export async function createUser(user: Users) {
-    const { error } = await supabase.from("users").insert(user);
+  const { error } = await supabase.from("users").insert(user);
 
-    if (error) {
-        console.error("Error creating user:", error.message);
-        return;
-    }
+  if (error) {
+    console.error("Error creating user:", error.message);
+    return;
+  }
 }
 
 export async function readUser(id: number): Promise<Users | null> {
-    const { data, error } = await supabase.from("users").select("*").eq("user_id", id).single();
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("user_id", id)
+    .single();
 
-    if (error) {
-        console.error("Error reading user:", error);
-        return null;
-    }
+  if (error) {
+    console.error("Error reading user:", error.message);
+    return null;
+  }
 
-    return data;
+  return data;
 }
 
 export async function updateUser(id: number, updatedUser: Partial<Users>) {
-    const { error } = await supabase.from("users").update(updatedUser).eq("id", id);
+  const { error } = await supabase
+    .from("users")
+    .update(updatedUser)
+    .eq("id", id);
 
-    if (error) {
-        console.error("Error updating user:", error);
-        return;
-    }
+  if (error) {
+    console.error("Error updating user:", error.message);
+    return;
+  }
 }
 
 export async function deleteUser(id: number) {
-    const { error } = await supabase.from("users").delete().eq("id", id);
+  const { error } = await supabase.from("users").delete().eq("id", id);
 
-    if (error) {
-        console.error("Error deleting user:", error);
-        return;
-    }
+  if (error) {
+    console.error("Error deleting user:", error.message);
+    return;
+  }
 }
