e2b7b296e84fc9d98db646b04059a7a86f00fdbf
vishal <vbalagani8@gmail.com>
2026-03-02 18:08:29 -0500

    questions crud

---
+++ src/actions/db/questions.ts
@@ -0,0 +1,67 @@
+import { NewQuestions, Questions } from "@/src/types";
+import { supabase } from "@/src/lib/supabase-client";
+
+export async function createQuestion(
+  question: NewQuestions,
+): Promise<Questions | null> {
+  const { data, error } = await supabase
+    .from("questions")
+    .insert(question)
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Error creating question: ", error.message);
+    return null;
+  }
+
+  return data as Questions;
+}
+
+export async function readQuestion(id: number): Promise<Questions | null> {
+  const { data, error } = await supabase
+    .from("questions")
+    .select()
+    .eq("question_id", id)
+    .maybeSingle();
+
+  if (error) {
+    console.error("Error fetching questions: ", error.message);
+    return null;
+  }
+
+  return data as Questions;
+}
+
+export async function updateQuestion(
+  id: number,
+  question: Partial<NewQuestions>,
+): Promise<Questions | null> {
+  const { data, error } = await supabase
+    .from("questions")
+    .update(question)
+    .eq("question_id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error("Error updating question: ", error.message);
+    return null;
+  }
+
+  return data as Questions;
+}
+
+export async function deleteQuestion(id: number): Promise<boolean> {
+  const { error } = await supabase
+    .from("questions")
+    .delete()
+    .eq("question_id", id);
+
+  if (error) {
+    console.error("Error deleting question: ", error.message);
+    return false;
+  }
+
+  return true;
+}
