e3ba1f35c256361d1a6424d2760d1c30ce503d68
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-03-02 16:41:26 -0500

    updated latest migration

--- supabase/migrations/20260302202324_remote_schema.sql
+++ supabase/migrations/20260302202324_remote_schema.sql
@@ -1,13 +1,22 @@
-alter table "public"."campaign_members" alter column "user_id" set default auth.uid();
+alter table "public"."campaign_members"
+  drop constraint if exists "campaign_members_user_id_fkey";
+alter table "public"."campaign_members"
+  drop constraint if exists "campaign_members_pkey";
 
-alter table "public"."campaign_members" alter column "user_id" drop identity;
+alter table "public"."campaign_members" drop column if exists "user_id";
+alter table "public"."campaign_members"
+  add column "user_id" uuid not null default auth.uid();
 
-alter table "public"."campaign_members" alter column "user_id" set data type uuid using "user_id"::uuid;
+alter table "public"."users" drop constraint if exists "users_pkey";
+alter table "public"."users" drop column if exists "id";
 
-alter table "public"."users" alter column "id" set default auth.uid();
+alter table "public"."users"
+  add column "id" uuid not null default auth.uid() primary key references auth.users(id) on delete cascade;
 
-alter table "public"."users" alter column "id" drop identity;
+alter table "public"."campaign_members"
+  add constraint "campaign_members_user_id_fkey"
+  foreign key ("user_id") references "public"."users"("id") on delete cascade;
 
-alter table "public"."users" alter column "id" set data type uuid using "id"::uuid;
-
-
+alter table "public"."campaign_members"
+  add constraint "campaign_members_pkey"
+  primary key ("user_id", "campaign_id");
