939603c31a6917c3ce2609ba8983653eb44b6fca
Richard Thomas <richuthomasp@gmail.com>
2026-03-01 19:20:26 -0500

    Fetch answer operation

--- src/actions/db/answers.ts
+++ src/actions/db/answers.ts
@@ -3,10 +3,27 @@ import { supabase } from "@/src/lib/supabase-client";
 
 
 export async function createAnswer(answer: NewAnswer){
-  const { error } = await supabase.from("answers").insert(answer);
+  const { error } = await supabase
+    .from("answers")
+    .insert(answer);
   
   if (error) {
     console.error("Error creating answer:", error.message);
     return;
   }
+}
+
+export async function readAnswer(answerId: number): Promise<Answer | null> {
+  const { data, error } = await supabase
+    .from("answers")
+    .select() // select all columns of answer
+    .eq("answer_id", answerId) // finding the wanted answer
+    .maybeSingle();
+
+    if (error) {
+      console.error("Error fetching answer:", error.message);
+      return null;
+    }
+
+    return data;
 }
