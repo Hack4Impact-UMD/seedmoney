4714c27494349c49d370b14b008eb3f896460b5b
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-04-03 16:36:24 -0400

    Added migration

---
+++ supabase/migrations/20260403203054_remote_schema.sql
@@ -0,0 +1,9 @@
+alter table "public"."campaigns" add column "donors" bigint;
+
+alter table "public"."campaigns" add column "givebutter_id" text;
+
+alter table "public"."campaigns" add column "givebutter_slug" text;
+
+alter table "public"."campaigns" add column "raised" bigint;
+
+
