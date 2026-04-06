5cbf3207c0ac47bad4e6ea94e449e10431da6346
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-04-06 17:33:22 -0400

    Added competition_metadata table and transactions table + types

---
+++ src/types/db/competitionMetadata.ts
@@ -0,0 +1,5 @@
+export type CompetitionMetadata = {
+  competition_id: number,
+  start_date: string,
+  end_date: string
+}
---
+++ src/types/db/transactions.ts
@@ -0,0 +1,12 @@
+export interface Transaction {
+  transaction_id: number;
+  first_name: string;
+  last_name: string;
+  campaign_id: number;
+  amount_donated: number;
+  phone: string,
+  email: string;
+  date: string;
+  total_paid: number;
+  status: string;
+}
