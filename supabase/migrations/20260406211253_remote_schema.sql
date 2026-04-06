1500d4a6ee08fee74592c396ba19350127535d8f
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-04-06 17:14:51 -0400

    added table changes

---
+++ supabase/migrations/20260406211253_remote_schema.sql
@@ -0,0 +1,11 @@
+alter table "public"."answers" disable row level security;
+
+alter table "public"."campaign_members" disable row level security;
+
+alter table "public"."campaigns" disable row level security;
+
+alter table "public"."questions" disable row level security;
+
+alter table "public"."users" disable row level security;
+
+
