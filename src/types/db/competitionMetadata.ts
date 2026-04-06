9833b726f854c400e5bb9dd1adfc10bc449d208e
Esha Vigneswaran <eshavigneswaran@gmail.com>
2026-04-06 17:42:03 -0400

    Updates comp metadata types

--- src/types/db/competitionMetadata.ts
+++ src/types/db/competitionMetadata.ts
@@ -1,5 +1,6 @@
 export type CompetitionMetadata = {
   competition_id: number,
   start_date: string,
-  end_date: string
+  end_date: string,
+  is_current: boolean
 }
---
+++ supabase/migrations/20260406214043_remote_schema.sql
@@ -0,0 +1,3 @@
+alter table "public"."competition_metadata" add column "is_current" boolean;
+
+
