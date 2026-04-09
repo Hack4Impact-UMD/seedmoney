5dcbd5e117c53280607b232fe723d793f1ce2af9
Samarth Kolanupaka <samarth212@gmail.com>
2026-04-09 13:43:52 -0400

    createCampaign hook

---
+++ src/hooks/campaigns/useCreateCampaigns.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCampaign } from "@/src/actions/db/campaigns";
+import { Campaign } from "@/src/types/db/campaigns";
+
+export type CreateCampaignInput = Omit<
+  Campaign,
+  "campaign_id" | "givebutterlink" | "givebutter_id" | "givebutter_slug"
+> & {
+  givebutterlink: string | undefined;
+  givebutter_id?: string | undefined;
+  givebutter_slug?: string | undefined;
+};
+
+export default function useCreateCampaigns() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (campaignData: CreateCampaignInput): Promise<Campaign> => {
+      const campaign = await createCampaign(campaignData);
+
+      if (!campaign) {
+        throw new Error("Error creating campaign");
+      }
+
+      return campaign;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["campaigns"] });
+    },
+  });
+}
