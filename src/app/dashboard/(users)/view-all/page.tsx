c9ff7a35941390f050ac5d52a0130c1ae9d396d0
Samarth Kolanupaka <samarth212@gmail.com>
2026-05-20 15:17:47 -0400

    fix filter layout

--- src/app/dashboard/(users)/view-all/page.tsx
+++ src/app/dashboard/(users)/view-all/page.tsx
@@ -6,8 +6,12 @@ import { useAuth } from "@/src/context/AuthProvider";
 import Error from "@/src/app/error";
 import Loading from "@/src/app/loading";
 import useReadPreviousChallengeApplications from "@/src/hooks/campaigns/table/useReadPreviousChallengeApplications";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function ViewAllCampaignsPage() {
+  const router = useRouter();
   const { user } = useAuth();
   const {
     data: campaigns = [],
@@ -28,13 +32,22 @@ export default function ViewAllCampaignsPage() {
       <div className="flex-1 bg-[#F6FAF9] px-4 py-8 pb-24 md:px-10 md:py-[60px] md:pb-[60px]">
         <div className="flex w-full flex-col gap-6">
           <div className="hidden w-full md:block">
+            <Button
+              startIcon={<ArrowBackIcon />}
+              onClick={() => router.push("/dashboard")}
+              className="!mb-4 !px-0 !text-[18px] !font-bold !uppercase !text-[#6A6A6A]"
+            >
+              Back
+            </Button>
             <h1 className="text-4xl font-bold text-[#096B2E]">All Campaigns</h1>
           </div>
 
           <CampaignsTable
             initialData={campaigns}
             pageTitle="All Campaigns"
-            pageListLabel="All Campaigns List"
+            pageListLabel="Campaign List"
+            useFilterMenu
+            desktopSearchPlaceholder="Campaign Title, Campaign Leader, etc..."
           />
         </div>
       </div>
